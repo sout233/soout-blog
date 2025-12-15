5838acc7c070ed9200bc257faa3bbe61aa972064
sout_Nantang <sout233@163.com>
2025-12-16 00:03:46 +0800

    fix: disable eslint sth

--- src/lib/server/supabaseClient.ts
+++ src/lib/server/supabaseClient.ts
@@ -5,4 +5,5 @@ const supabaseKey = process.env.SUPABASE_KEY;
 if (!supabaseKey) {
 	throw new Error('Missing SUPABASE_KEY environment variable');
 }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const supabase = createClient(supabaseUrl, supabaseKey);
