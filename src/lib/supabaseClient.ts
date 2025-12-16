7fd3eeec244f1d0fd85ed413e0ea3e719f242eb8
sout_Nantang <sout233@163.com>
2025-12-16 21:36:23 +0800

    chore: move key to .env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabaseUrl = 'https://ffcfpgtzwjxpowzogbpf.supabase.co';
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmY2ZwZ3R6d2p4cG93em9nYnBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MDQ4NzIsImV4cCI6MjA4MTM4MDg3Mn0.xvmUqSiPzoainhH8Y3KB-GBCf6LpUTtK8tUrq8l2zNY';
-if (!supabaseKey) {
-	throw new Error('Missing SUPABASE_KEY environment variable');
+if (!PUBLIC_SUPABASE_URL || !PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error('Missing Supabase environment variables');
 }
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
