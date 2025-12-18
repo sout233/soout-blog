b2ea29ac66c528269e01ccaa71b8bb0a7f955f9c
sout_Nantang <sout233@163.com>
2025-12-18 10:48:28 +0800

    idk: just adjust svg scale

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,7 +12,7 @@
 		{#snippet icon()}
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
-				class="scale-80"
+				class="translate-y-[0.5px] scale-70"
 				width="12"
 				height="24"
 				viewBox="0 0 12 24"
